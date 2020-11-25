d88c61afceab8da0090dc4987e9994a71b00f2ae
Durval Carvalho <durvalcsouza@outlook.com>
2020-11-24 23:05:05 -0300

    Fix whitenoise configuration

--- reliability/settings.py
+++ reliability/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = "/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 CACHES = {
     'default': {
